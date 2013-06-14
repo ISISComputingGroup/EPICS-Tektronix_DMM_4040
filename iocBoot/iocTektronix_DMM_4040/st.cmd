af46fc48c7587b7569fafd7a0405745d71fb563b
Matt Clarke <matt.clarke@stfc.ac.uk>
2013-06-14 07:57:46 +0000

    Updated protocol file to work with ndxchipir (real directory path)
    Former-commit-id: e5d50630a84f75390a3557dbf0261bdd96d919d5

--- iocBoot/iocTektronix_DMM_4040/st.cmd
+++ iocBoot/iocTektronix_DMM_4040/st.cmd
@@ -11,8 +11,8 @@ cd ${TOP}
 dbLoadDatabase "dbd/Tektronix_DMM_4040.dbd"
 Tektronix_DMM_4040_registerRecordDeviceDriver pdbbase
 
-lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Tektronix_DMM_4040App/protocol/Tektronix_DMM_4040.xml", "", 0)
-#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Tektronix_DMM_4040App/protocol/Tektronix_DMM_4040.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
+#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Tektronix_DMM_4040App/protocol/Tektronix_DMM_4040.xml", "", 0)
+lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Tektronix_DMM_4040App/protocol/Tektronix_DMM_4040.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
 
 ## Load record instances
 dbLoadRecords("db/Tektronix_DMM_4040.db","P=ex1:")
@@ -20,5 +20,3 @@ dbLoadRecords("db/Tektronix_DMM_4040.db","P=ex1:")
 cd ${TOP}/iocBoot/${IOC}
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=kht13119Host"
